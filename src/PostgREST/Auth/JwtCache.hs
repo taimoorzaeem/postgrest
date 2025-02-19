cbea41eb6593d1a6e530aed0c423e531264f2eb1
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2025-02-19 11:58:02 +0500

    feat: add metric pgrst_jwt_cache_size_bytes in admin server

--- src/PostgREST/Auth/JwtCache.hs
+++ src/PostgREST/Auth/JwtCache.hs
@@ -4,7 +4,6 @@ Description : PostgREST Jwt Authentication Result Cache.
 
 This module provides functions to deal with the JWT cache
 -}
-{-# LANGUAGE NamedFieldPuns #-}
 module PostgREST.Auth.JwtCache
   ( init
   , JwtCacheState
@@ -16,29 +15,37 @@ import qualified Data.Aeson.KeyMap as KM
 import qualified Data.Cache        as C
 import qualified Data.Scientific   as Sci
 
+import Data.Maybe            (fromJust)
 import Data.Time.Clock       (UTCTime, nominalDiffTimeToSeconds)
 import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
 import System.Clock          (TimeSpec (..))
+import System.IO.Unsafe      (unsafePerformIO)
 
-import PostgREST.Auth.Types (AuthResult (..))
-import PostgREST.Error      (Error (..))
+import PostgREST.Auth.Types  (AuthResult (..))
+import PostgREST.Error       (Error (..))
+import PostgREST.Internal    (recursiveSizeNF)
+import PostgREST.Observation (Observation (..), ObservationHandler)
 
+import Control.Debounce
 import Protolude
 
-newtype JwtCacheState = JwtCacheState
-  { jwtCache :: C.Cache ByteString AuthResult
+data JwtCacheState = JwtCacheState
+  -- | Jwt Cache
+  { jwtCache                     :: C.Cache ByteString AuthResult
+  -- | Calculate cache size with debounce
+  , cacheSizeCalcDebounceTimeout :: MVar (IO ())
   }
 
 -- | Initialize JwtCacheState
 init :: IO JwtCacheState
 init = do
   cache <- C.newCache Nothing -- no default expiration
-  return $ JwtCacheState cache
+  JwtCacheState cache <$> newEmptyMVar
 
 -- | Used to retrieve and insert JWT to JWT Cache
-lookupJwtCache :: JwtCacheState -> ByteString -> Int -> IO (Either Error AuthResult) -> UTCTime -> IO (Either Error AuthResult)
-lookupJwtCache JwtCacheState{jwtCache} token maxLifetime parseJwt utc = do
-  checkCache <- C.lookup jwtCache token
+lookupJwtCache :: JwtCacheState -> ByteString -> Int -> IO (Either Error AuthResult) -> UTCTime -> ObservationHandler -> IO (Either Error AuthResult)
+lookupJwtCache jwtCacheState token maxLifetime parseJwt utc observer = do
+  checkCache <- C.lookup (jwtCache jwtCacheState) token
   authResult <- maybe parseJwt (pure . Right) checkCache
 
   case (authResult,checkCache) of
@@ -56,13 +63,17 @@ lookupJwtCache JwtCacheState{jwtCache} token maxLifetime parseJwt utc = do
 
     (Right res, Nothing) -> do -- cache miss
 
+      -- get expiration time
       let timeSpec = getTimeSpec res maxLifetime utc
 
       -- purge expired cache entries
-      C.purgeExpired jwtCache
+      C.purgeExpired (jwtCache jwtCacheState)
 
       -- insert new cache entry
-      C.insert' jwtCache (Just timeSpec) token res
+      C.insert' (jwtCache jwtCacheState) (Just timeSpec) token res
+
+      -- calculate cache size with debounce and log it
+      updateCacheSizeWithDebounce jwtCacheState observer
 
     _                    -> pure ()
 
@@ -77,3 +88,57 @@ getTimeSpec res maxLifetime utc = do
   case expireJSON of
     Just (JSON.Number seconds) -> TimeSpec (sciToInt seconds - utcToSecs utc) 0
     _                          -> TimeSpec (fromIntegral maxLifetime :: Int64) 0
+
+-- | Update JwtCacheSize Metric
+--
+-- Runs the cache size calculation with debounce
+updateCacheSizeWithDebounce :: JwtCacheState -> ObservationHandler -> IO ()
+updateCacheSizeWithDebounce jwtCacheState observer = do
+  cSizeDebouncer <- tryReadMVar $ cacheSizeCalcDebounceTimeout jwtCacheState
+  case cSizeDebouncer of
+    Just d -> d
+    Nothing -> do
+      newDebouncer <-
+        mkDebounce defaultDebounceSettings
+          -- debounceFreq is set to default 1 second
+          { debounceAction = calculateSizeThenLog
+          , debounceEdge = leadingEdge -- logs at the start and the end
+          }
+      putMVar (cacheSizeCalcDebounceTimeout jwtCacheState) newDebouncer
+      newDebouncer
+    where
+      calculateSizeThenLog :: IO ()
+      calculateSizeThenLog = do
+        size <- calcCacheSizeInBytes (jwtCache jwtCacheState)
+        observer $ JwtCache size -- updates and logs the metric
+
+-- | Calculate JWT Cache Size in Bytes
+--
+-- The cache size is updated by calculating the size of every
+-- cache entry and updating the metric.
+--
+-- The cache entry consists of
+--   key          :: ByteString
+--   value        :: AuthReult
+--   expire value :: TimeSpec
+--
+-- We calculate the size of each cache entry component
+-- by using recursiveSizeNF function which first evaluates
+-- the data structure to Normal Form and then calculate size.
+-- The normal form evaluation is necessary for accurate size
+-- calculation because haskell is lazy and we dont wanna count
+-- the size of large thunks (unevaluated expressions)
+calcCacheSizeInBytes :: C.Cache ByteString AuthResult -> IO Int
+calcCacheSizeInBytes cache = do
+  cacheList <- C.toList cache
+  let szList = [ unsafePerformIO (getSize (bs, ar, fromJust ts)) | (bs, ar, ts) <- cacheList]
+  return $ fromIntegral (sum szList)
+    where
+      getSize :: (ByteString, AuthResult, TimeSpec) -> IO Word
+      getSize (bs, ar, ts) = do
+        keySize      <- recursiveSizeNF bs
+        arClaimsSize <- recursiveSizeNF $ authClaims ar
+        arRoleSize   <- recursiveSizeNF $ authRole ar
+        timeSpecSize <- liftA2 (+) (recursiveSizeNF (sec ts)) (recursiveSizeNF (nsec ts))
+
+        return (keySize + arClaimsSize + arRoleSize + timeSpecSize)
